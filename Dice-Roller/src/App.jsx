85e9609a5fad09dfd6a3e97be5d261b1b74c343b
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-02-06 21:16:33 +0100

    Syntax corrected

--- Dice-Roller/src/App.jsx
+++ Dice-Roller/src/App.jsx
@@ -20,7 +20,6 @@ function App() {
       setRollResults(currentResults[currentResults.length - 1]);
     }
 
-    
     const multipleRollDiceValue = newResult.reduce(
       (accumulator, value) => accumulator + value.roll,
       0
