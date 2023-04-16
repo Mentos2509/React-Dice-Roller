ed00697c962ab019f442c55c0a22a57a7c197423
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-04-16 11:27:28 +0200

    corrected syntax

--- src/theme/ThemeConfig.js
+++ src/theme/ThemeConfig.js
@@ -1,28 +1,27 @@
-
 export const ThemeConfig = {
-    font: {
-      family: "'Roboto', sans-serif",
-    },
-  
-    colors: {
-      white: "hsl(225, 100%, 100%)",
-      light: "hsl(232, 6%, 90%)",
-      gray: "hsl(231, 11%, 63%)",
-      primary: "hsl(240, 2%, 10%)",
-      secondary: "hsl(206, 94%, 87%)",
-      danger: "hsl(354, 84%, 57%)",
-      green: "hsl(135, 15%, 58%);",
-    },
-    sizes: {
-      base: "10px",
-      xs: "1.4rem",
-      s: "1.6rem",
-      m: "1.8rem",
-      l: "2.0rem",
-      xl: "3.0rem",
-    },
-    weights: {
-      light: "300",
-      bold: "700",
-    },
-  };
+  font: {
+    family: "'Roboto', sans-serif",
+  },
+
+  colors: {
+    white: "hsl(225, 100%, 100%)",
+    light: "hsl(232, 6%, 90%)",
+    gray: "hsl(231, 11%, 63%)",
+    primary: "hsl(240, 2%, 10%)",
+    secondary: "hsl(206, 94%, 87%)",
+    danger: "hsl(354, 84%, 57%)",
+    green: "hsl(135, 15%, 58%);",
+  },
+  sizes: {
+    base: "10px",
+    xs: "1.4rem",
+    s: "1.6rem",
+    m: "1.8rem",
+    l: "2.0rem",
+    xl: "3.0rem",
+  },
+  weights: {
+    light: "300",
+    bold: "700",
+  },
+};
